0da913a2b7566d53f309a15e7be559174faee61e
Anderson De Oliveira Madeira <anderson.madeira@geq.com.br>
2017-03-29 13:59:13 -0300

    Now displaying completed tasks that were archived.

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ angular.module('tasksApp', [])
   .controller('TaskListController', function() {
     var ctrl = this;
     ctrl.taskText = "";
+    ctrl.archives = [];
     ctrl.tasks = [
       {text:'Learn AngularJS', done:true, createdAt:new Date(), completedAt:new Date()},
-      {text:'Build an AngularJS app', done:false, createdAt:new Date(), completedAt:null}];
+      {text:'Build an AngularJS app', done:false, createdAt:new Date(), completedAt:'-'}];
 
     ctrl.addTask = function() {
       if (ctrl.taskText != "") {
-        ctrl.tasks.push({text:ctrl.taskText, done:false, createdAt:new Date(), completedAt:null});
+        ctrl.tasks.push({text:ctrl.taskText, done:false, createdAt:new Date(), completedAt:'-'});
         ctrl.taskText = '';
       }
     };
@@ -17,7 +18,7 @@ angular.module('tasksApp', [])
       if (done) {
         task.completedAt = new Date();
       } else {
-        task.completedAt = null;
+        task.completedAt = '-';
       }
     }
 
@@ -34,6 +35,7 @@ angular.module('tasksApp', [])
       ctrl.tasks = [];
       angular.forEach(oldTasks, function(task) {
         if (!task.done) ctrl.tasks.push(task);
+        else ctrl.archives.push(task);
       });
     };
   });
