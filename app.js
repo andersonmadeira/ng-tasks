be4fe3c07b4afd4d69033399432f227198413b58
Anderson De Oliveira Madeira <anderson.madeira@geq.com.br>
2017-03-29 15:04:22 -0300

    You can now remove completed tasks and local storage

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-angular.module('tasksApp', [])
-  .controller('TaskListController', function() {
+angular.module('tasksApp', ['ngStorage'])
+  .controller('TaskListController', ['$scope', '$localStorage', 'Util', function($scope, $localStorage, Util) {
     var ctrl = this;
     ctrl.taskText = "";
-    ctrl.archives = [];
-    ctrl.tasks = [
-      {text:'Learn AngularJS', done:true, createdAt:new Date(), completedAt:new Date()},
-      {text:'Build an AngularJS app', done:false, createdAt:new Date(), completedAt:'-'}];
+
+    ctrl.tasks = $localStorage.tasks || [
+      {id:Util.genRandomId(), text:'Learn AngularJS', done:true, createdAt:new Date(), completedAt:new Date()},
+      {id:Util.genRandomId(), text:'Build an AngularJS app', done:false, createdAt:new Date(), completedAt:'-'}];
+
+    ctrl.archives = $localStorage.archives || [];
+
+    $localStorage.tasks = ctrl.tasks;
+    $localStorage.archives = ctrl.archives;
 
     ctrl.addTask = function() {
       if (ctrl.taskText != "") {
-        ctrl.tasks.push({text:ctrl.taskText, done:false, createdAt:new Date(), completedAt:'-'});
+        var new_task = {id:Util.genRandomId(), text:ctrl.taskText, done:false, createdAt:new Date(), completedAt:'-'};
+        ctrl.tasks.push(new_task);
         ctrl.taskText = '';
       }
     };
@@ -37,5 +43,16 @@ angular.module('tasksApp', [])
         if (!task.done) ctrl.tasks.push(task);
         else ctrl.archives.push(task);
       });
+      $localStorage.tasks = ctrl.tasks;
+      $localStorage.archives = ctrl.archives;
+    };
+
+    ctrl.removeCompleted = function(task) {
+      var index = ctrl.archives.map(function(e) {return e.id; }).indexOf(task.id);
+      ctrl.archives.splice(index, 1);
+    }
+  }]).service('Util', function() {
+    this.genRandomId = function (length=20) {
+      return Math.random().toString(36).substr(2, length);
     };
   });
