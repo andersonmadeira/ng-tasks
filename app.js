30163548cd5fda3b100c6b3e134cc1b35d39a932
Anderson De Oliveira Madeira <anderson.madeira@geq.com.br>
2017-03-29 13:07:17 -0300

    Added created and completed dates for tasks

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ angular.module('tasksApp', [])
     var ctrl = this;
     ctrl.taskText = "";
     ctrl.tasks = [
-      {text:'Learn AngularJS', done:true},
-      {text:'Build an AngularJS app', done:false}];
+      {text:'Learn AngularJS', done:true, createdAt:new Date(), completedAt:new Date()},
+      {text:'Build an AngularJS app', done:false, createdAt:new Date(), completedAt:null}];
 
     ctrl.addTask = function() {
       if (ctrl.taskText != "") {
-        ctrl.tasks.push({text:ctrl.taskText, done:false});
+        ctrl.tasks.push({text:ctrl.taskText, done:false, createdAt:new Date(), completedAt:null});
         ctrl.taskText = '';
       }
     };
+
+    ctrl.completeTask = function(task, done) {
+      if (done) {
+        task.completedAt = new Date();
+      } else {
+        task.completedAt = null;
+      }
+    }
 
     ctrl.remaining = function() {
       var count = 0;
